ac0adc84bd61c35f2b3a7efac3cb65d62beda508
FrostXk9 <brandonroulstone465@gmail.com>
2024-03-17 17:29:01 +0200

    took middleware off cart route note that it works using localhost but with render it does not update it

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.delete('/logout', (req, res) => {
 });
 
 app.use('/products', productsRoute);
-app.use('/cart',verifyJwt, cartRoute);
+app.use('/cart', cartRoute);
 app.use('/users', userRoute);
 
 app.listen(PORT, console.log(`server running on http://localhost:${PORT}`));
