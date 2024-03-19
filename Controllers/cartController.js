0dd87c5a381a63fb956f4c881bbced6abb979304
brandonRoulstone <brandonroulstone465@gmail.com>
2024-03-19 09:49:16 +0200

    add to cart

--- Controllers/cartController.js
+++ Controllers/cartController.js
@@ -1,16 +1,19 @@
-import { getCart, addToCart, insert, removeFromCart } from "../Model/db.js";
+import { getCart, addToCart, insert, removeFromCart, getProductByID } from "../Model/db.js";
 
 export default {
     allCartItems : async (req, res) => {
         res.send(await getCart())
     },
     addToCartTable : async (req, res) => {
-        console.log(req.query);
-        console.log(+req.params.id);
         let { quantity } = req.body
+        const [product] = await getProductByID(+req.params.id);
+        const [cart] = await getCart();
         let { user_id } = req.query
         await insert(+req.params.id, user_id, quantity)
-        res.send(await getCart())
+        res.send({
+            product : product,
+            cart : cart
+        });
     },
     deleteFromCart : async (req, res) => {
         await removeFromCart(+req.params.id)
